f414481e4a05c2ca34c0409254cac0cff8343234
Abdurizo <103021939+abdurizo@users.noreply.github.com>
2025-09-17 17:57:59 +0500

    tab

--- script/fiterImg.js
+++ script/fiterImg.js
@@ -4,6 +4,11 @@ productDemons.addEventListener('click', function (event) {
     const productBigImgs = productDemons.querySelectorAll('.big_img');
     const clickProductImg = event.target.closest('.product_image_card');
 
+    const productImgAll=productDemons.querySelectorAll('.product_image_card');
+    productImgAll.forEach(el => {
+      el.classList.remove('active')
+    })
+
     if (!clickProductImg) return;
     productBigImgs.forEach(el => {
         el.classList.remove('active')
@@ -14,4 +19,5 @@ productDemons.addEventListener('click', function (event) {
     if (activeBigImg) {
          activeBigImg.classList.add('active');
     }
+    clickProductImg.classList.add('active')
 })
